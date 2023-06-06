9673027e9b09d757cdea5b8aa21a740c697075dc
Manasi <manasitilak12@gmail.com>
2023-06-06 17:35:21 +0530

    made the json readable

--- posts.py
+++ posts.py
@@ -6,7 +6,6 @@ from google.analytics.data_v1beta.types import (
     DateRange,
     Dimension,
     Metric,
-    MetricType,
     RunReportRequest,
 )
 
@@ -24,40 +23,20 @@ def run_report(property_id=PROPERTY):
         metrics=[Metric(name="activeUsers")],
         date_ranges=[DateRange(start_date="90daysAgo", end_date="today")],
         limit=5,
-        # offset=100000,
     )
     response = client.run_report(request)
+    # print(response)
     write_json(response)
-
-def print_run_report_response(response):
-    """Prints results of a runReport call."""
-    print(f"{response.row_count} rows received")
-    for dimensionHeader in response.dimension_headers:
-        print(f"Dimension header name: {dimensionHeader.name}")
-    for metricHeader in response.metric_headers:
-        metric_type = MetricType(metricHeader.type_).name
-        print(f"Metric header name: {metricHeader.name})")
-
-    print("Report result:")
-    for rowIdx, row in enumerate(response.rows):
-        print(f"\nRow {rowIdx}")
-        for i, dimension_value in enumerate(row.dimension_values):
-            dimension_name = response.dimension_headers[i].name
-            print(f"{dimension_name}: {dimension_value.value}")
-
-        for i, metric_value in enumerate(row.metric_values):
-            metric_name = response.metric_headers[i].name
-            print(f"{metric_name}: {metric_value.value}")
 
 def write_json(response):
     with open('data/posts.json', "w")as f:
         for rowIdx, row in enumerate(response.rows):
-            f.write(f"\nRow {rowIdx}")
+            f.write("\n")
             for i, dimension_value in enumerate(row.dimension_values):
-                dimension_name = response.dimension_headers[i].name
-                f.write(f"{dimension_name}: {dimension_value.value}")
+                f.write(f"{dimension_value.value}|")
 
             for i, metric_value in enumerate(row.metric_values):
+                f.write("\t\t")
                 metric_name = response.metric_headers[i].name
                 f.write(f"{metric_name}: {metric_value.value}")
 
